610341384b9b33da7f711ed385089b11b7be6873
SpezialK-dev <spezcialK@protonmail.com>
2021-06-24 15:29:19 +0200

    added a comment

--- src/main/java/Encyptor/cipher/Output.java
+++ src/main/java/Encyptor/cipher/Output.java
@@ -3,6 +3,8 @@ package Encyptor.cipher;
 
 //useless class only here because I need a new return type
 
+
+//might add password and other stuff here in the future depending on how usefull that will be 
 public class Output {
     private byte[] iv;
     private byte[] salt;
