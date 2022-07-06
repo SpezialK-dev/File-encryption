880e0a7442fc8d0886af7e883a53ff028a63ae81
SpezialK-dev <spezcialK@protonmail.com>
2022-07-06 12:20:20 +0200

    fixed code crashing when creating a new window

--- src/main/java/Encyptor/Gui.java
+++ src/main/java/Encyptor/Gui.java
@@ -1,21 +1,54 @@
 package Encyptor;
 
+import imgui.flag.ImGuiWindowFlags;
+import imgui.flag.ImGuiWindowFlags.*;
+import imgui.type.ImBoolean;
+import imgui.type.ImBoolean.*;
 import imgui.ImGui;
 import imgui.app.Application;
 import imgui.app.Configuration;
+import imgui.app.Window;
 
+
+//this is the new Gui there to replace the old Jpanel one
 public class Gui extends Application{
+    //this is the window for all the encryption stuff
 
     @Override
     protected void configure(Configuration config) {
         config.setTitle("File-encryption");
     }
 
-
-    @Override
-    public void process() {
-        ImGui.text("Hello, World!");
+    public void process(){
+        //todo fix this to work so that I can actually open a new window
+        // Create a window called "My First Tool", with a menu bar.
+        ImBoolean pOpen = new ImBoolean(true);
+        testWindow();
     }
-    //This will be the replacement for the old
 
+
+    private void testWindow(){
+        //todo fix this to work so that I can actually open a new window
+        //Create a window called "My First Tool", with a menu bar.
+        ImBoolean pOpen = new ImBoolean(true);
+        ImGui.begin("Encryption",pOpen,ImGuiWindowFlags.MenuBar );
+        if (ImGui.beginMenuBar()) {
+            if (ImGui.beginMenu("File")) {
+                if (ImGui.menuItem("Open..", "Ctrl+O")) {
+                    fileOpend("test"); }
+                if (ImGui.menuItem("Close", "Ctrl+W")) {
+                }
+                ImGui.endMenu();
+            }
+            ImGui.endMenuBar();
+            if(ImGui.button("Encrypt")){
+
+            }
+        }
+        ImGui.end();
+    }
+    // takes care of File selection
+    private void fileOpend(String filenameAndPath){
+        ImGui.text("CurrentFile Opened: ");
+    }
 }
